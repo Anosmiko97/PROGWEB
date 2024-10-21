315047b441fbf440165cb6061270d490a4664608
UrieL0x <143738012+UriL0x@users.noreply.github.com>
2024-10-21 14:06:58 -0700

    me rquivoque de carpeta XD

---
+++ tarea14/js/index.js
@@ -0,0 +1,57 @@
+const buttons = document.getElementsByClassName("button")
+const output = document.querySelector(".output");
+const history = document.querySelector("#history");
+const showBtn = document.querySelector("#show-btn");
+const list = document.querySelector("#list-cal");
+let input = "";
+let listCal = [];
+
+// Calculadora
+function calculate(operation) {
+    try {
+        let cal = eval(operation);
+        listCal.push(operation + " = " + cal);
+        input = "";
+        return cal;
+    } catch {
+        output.textContent = "SINTAX ERROR";
+    }
+}
+
+for (let btn of buttons) {
+    btn.addEventListener("click", () => {
+        console.log(btn.textContent);
+        if (btn.textContent === "=") {
+            output.textContent = calculate(input)
+        } else if (btn.textContent === "C") {
+            input = ""
+            output.textContent = ""
+        } else {
+            input += btn.textContent;
+            output.textContent = input;
+        }
+    })
+}
+
+// Historial del calculos
+let show = false;
+showBtn.addEventListener("click", () => {
+    if (show === false) {
+        if (listCal.length === 0) {
+            alert("No hay nada que mostrar");
+        } else {
+            show = true
+            history.style.opacity = "1";
+            for (let i of listCal) {
+                let div = document.createElement("p")
+                div.textContent = i;
+                list.appendChild(div);
+            } 
+        }
+    } else {
+        show = false
+        history.style.opacity = "0";
+        list.innerHTML = "";
+    }
+});
+
